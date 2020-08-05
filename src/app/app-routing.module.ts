66b0a2fb0d2f15ec80b350dc1c903faa469f1f13
SugLearn <saranya.sakthivel06@gmail.com>
2020-08-05 15:00:01 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 const routes: Routes = [
-  {path:'', redirectTo:'employee', pathMatch:'full'},
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login',component: LoginComponent},
   {path:'employees', component: EmployeeListComponent},
   { path: 'add', component: CreateEmployeeComponent },
-  {path: 'update', component:UpdateEmployeeComponent}
+  {path: 'update', component:UpdateEmployeeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'logout',component: LogoutComponent}
 ];
 
 @NgModule({
